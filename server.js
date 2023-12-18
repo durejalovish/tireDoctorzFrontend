69417416112277398575d5e44185cdf2587d14d3
lovish dureja <lovihdureja0786@gmail.com>
2023-12-18 17:50:09 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/'));
+app.use(express.static('./dist/ngrx-app'));
 app.get('/*', function(req,res) {
-res.sendFile('index.html', {root:'dist/'});});
+res.sendFile('index.html', {root:'dist/ngrx-app'});});
 app.listen(process.env.PORT || 8080);
